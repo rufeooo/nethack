4649e2626718b9799b454dafe0cc970509806032
Alan <alan@rufe.org>
2018-12-09 18:27:15 -0800

    Play script updates

--- play.sh
+++ play.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-FILE=`date +%Y%m%d_%H%M`
-ttyrec -e src/nethack $FILE.rec 
+FILE=`date +%Y%m%d_%H%M_%s`
+SAVE_COUNT=`ls ~/nh/install/games/lib/nethackdir/save/ | wc -l`
+
+if [ $SAVE_COUNT -ne "0" ]
+then
+	echo "continue.."
+	SUFFIX=.rec
+else
+	echo "new.."
+	SUFFIX=_new.rec
+fi
+ttyrec -e src/nethack $FILE$SUFFIX
